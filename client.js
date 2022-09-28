b7386469a7ef7df0739e32f5951af6b382d8a01a
nicohsfu <marcnicohernandez@gmail.com>
2022-09-28 12:28:54 -0700

    sent my initials over to the server

--- client.js
+++ client.js
@@ -10,7 +10,8 @@ const connect = function() {
 
   conn.on("connect", () => {
     // code that does something when the connection is first established
-    console.log("Connected!");
+    console.log("Connection is successfully established!");
+    conn.write("Name: Nic");
   });
 
   // Update the connect function to handle incoming data and console.log it for the player.
