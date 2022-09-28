3168ff3aefe86c6c1a403069c03f10e1949071dc
nicohsfu <marcnicohernandez@gmail.com>
2022-09-28 12:32:56 -0700

    added a move up function but then commmented it out

--- client.js
+++ client.js
@@ -12,7 +12,12 @@ const connect = function() {
     // code that does something when the connection is first established
     console.log("Connection is successfully established!");
     conn.write("Name: Nic");
-    conn.write("Move: up");
+    // conn.write("Move: up");
+
+    //     "Move: up" - move up one square (unless facing down)
+    // "Move: down" - move down one square (unless facing up)
+    // "Move: left" - move left one square (unless facing right)
+    // "Move: right" - move left one square (unless facing left)
   });
 
   // Update the connect function to handle incoming data and console.log it for the player.
